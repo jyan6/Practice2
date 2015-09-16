8930e72f41b4846bea8a9d6d436731e118b16549
Junyu Yan <jyan6@studentmail.nait.ca>
2015-09-16 16:12:13 -0600

    commit

---
+++ Practice2Website/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Practice2Website.Startup))]
+namespace Practice2Website
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
